b359efad7640ff578537d783b62b4ba2fac3b16f
stachowb <stachow.bartlomiej@gmail.com>
2021-05-02 01:59:12 +0200

    added shifts signals

---
+++ semshifts/shifts/signals.py
@@ -0,0 +1,7 @@
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+from .models import Shift
+
+@receiver(pre_save, sender=Shift)
+def pre_save_calc_wages(sender, instance, created, **kwargs):
+    pass

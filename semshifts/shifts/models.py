69cf020cfce6dd39339f5cdb99eceb8ebcb2d11b
stachowb <stachow.bartlomiej@gmail.com>
2021-05-09 02:15:05 +0200

    added shifts signals

--- semshifts/shifts/models.py
+++ semshifts/shifts/models.py
@@ -8,8 +8,5 @@ class Shift(models.Model):
     clock_out = models.DateTimeField()
     km_driven = models.IntegerField(default=0)
 
-    # post_save
-    shift_length = models.CharField(max_length=20, blank=True)
-
     def __str__(self):
         return f"{self.driver} -- {self.clock_in} - {self.clock_out}"

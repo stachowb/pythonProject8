4c1991d1c8e3f399ae991630017b8d6f1acdb107
stachowb <stachow.bartlomiej@gmail.com>
2021-05-09 21:58:44 +0200

    added shifts signals

--- semshifts/shifts/views.py
+++ semshifts/shifts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
-from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import ListView, CreateView
 from .models import Shift
 from .forms import CreateShiftForm
@@ -13,12 +13,13 @@ class ShiftsList(ListView):
     template_name = "shifts/shift-list.html"
 
 
-class ShiftCreate(CreateView):
+class ShiftCreate(SuccessMessageMixin, CreateView):
     model = Shift
     template_name = "shift-add.html"
     form = CreateShiftForm
     fields = ['driver', 'clock_in', 'clock_out', 'km_driven']
     success_url = "/shifts/list"
+    success_message = "Shift has been created"
 
 
 

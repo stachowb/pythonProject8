486ea48b423a3eb6796782e00d6c7d3ed93f8971
stachowb <stachow.bartlomiej@gmail.com>
2021-05-10 22:06:56 +0200

    added shifts signals

--- semshifts/shifts/forms.py
+++ semshifts/shifts/forms.py
@@ -6,3 +6,10 @@ class CreateShiftForm(forms.Form):
     class Meta:
         model = Shift
         fields = ['driver', 'clock_in', 'clock_out', 'km_driven']
+
+        widgets = {
+            "driver": forms.TextInput(attrs={"class": "form-control"}),
+            "clock_in": forms.TextInput(attrs={"class": "form-control"}),
+            "clock_out": forms.TextInput(attrs={"class": "form-control"}),
+            "km_driven": forms.TextInput(attrs={"class": "form-control"})
+        }

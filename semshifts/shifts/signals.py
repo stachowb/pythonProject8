8144178f0ece9084867d0cab25f545be0bcacc55
stachowb <stachow.bartlomiej@gmail.com>
2021-05-10 23:23:48 +0200

    added shifts signals

--- semshifts/shifts/signals.py
+++ semshifts/shifts/signals.py
@@ -1,8 +1,11 @@
 from django.dispatch import receiver
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save
 from .models import Shift
 
 
-@receiver(pre_save, sender=Shift)
-def pre_save_calc_wages(sender, instance, created, **kwargs):
-    pass
+@receiver(post_save, sender=Shift)
+def post_save_calc_wages(sender, instance, created, **kwargs):
+    if created:
+        instance.above_200 = instance.km_driven - 200
+        instance.shift_length = instance.clock_out - instance.clock_in
+        instance.save()

22005b90aacc7a9aae1f0a3d9b905fff9b1bd199
stachowb <stachow.bartlomiej@gmail.com>
2021-05-10 23:51:47 +0200

    added shifts signals

--- semshifts/shifts/signals.py
+++ semshifts/shifts/signals.py
@@ -6,6 +6,6 @@ from .models import Shift
 @receiver(post_save, sender=Shift)
 def post_save_calc_wages(sender, instance, created, **kwargs):
     if created:
-        instance.above_200 = instance.km_driven - 200
+        instance.above_200 = instance.km_driven - 200 if instance.km_driven > 200 else instance.km_driven
         instance.shift_length = instance.clock_out - instance.clock_in
         instance.save()

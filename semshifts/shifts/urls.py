126c1c53e6ce5de7f814fb368a2405b8279f6dd4
stachowb <stachow.bartlomiej@gmail.com>
2021-05-19 00:37:51 +0200

    added shifts signals

--- semshifts/shifts/urls.py
+++ semshifts/shifts/urls.py
@@ -4,6 +4,6 @@ from .views import ShiftList, ShiftCreate, ShiftUpdate,ShiftDelete
 urlpatterns = [
     path('list/', ShiftList.as_view(), name="shifts-list"),
     path('add/', ShiftCreate.as_view(), name="shifts-add"),
-    path('edit/<int:pk>', ShiftUpdate.as_view(), name="shifts-update"),
-    path('delete/<int:pk>', ShiftDelete.as_view(), name="shifts-delete")
+    path('edit/<slug:slug>/', ShiftUpdate.as_view(), name="shifts-edit"),
+    path('delete/<slug:slug>/', ShiftDelete.as_view(), name="shifts-delete")
 ]
